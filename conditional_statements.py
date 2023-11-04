acf07ee8be29ed271f608945fcc2093bf279cf1b
Harika-yaraganti <harikayaraganti15@gmail.com>
2023-11-04 22:49:23 +0530

    message

--- conditional_statements.py
+++ conditional_statements.py
@@ -1,6 +1,13 @@
-age = 18
-if age>=18:
-    print('hello')
+marks = 100
+if marks>=100:
+    print("you are qualified")
 else:
-    print('not hello')
+    print("not qualified")
+
+kidage = 8
+if kidage>=8:
+    print("kid can allow")
+else:
+    print("kid is not allowed")
+
 
